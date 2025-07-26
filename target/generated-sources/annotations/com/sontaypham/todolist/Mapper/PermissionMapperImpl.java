4600d8c5cebec6bb3cddb8632cf85c6a010cb268
sontaypham <phamsontay2206@gmail.com>
2025-07-26 21:02:07 +0700

    test(Permission-ServiceIT) : add test for permission service

--- target/generated-sources/annotations/com/sontaypham/todolist/Mapper/PermissionMapperImpl.java
+++ target/generated-sources/annotations/com/sontaypham/todolist/Mapper/PermissionMapperImpl.java
@@ -8,8 +8,8 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-07-26T20:35:16+0700",
-    comments = "version: 1.5.5.Final, compiler: javac, environment: Java 23.0.2 (Oracle Corporation)"
+    date = "2025-07-26T20:52:16+0700",
+    comments = "version: 1.5.5.Final, compiler: javac, environment: Java 21.0.7 (Red Hat, Inc.)"
 )
 @Component
 public class PermissionMapperImpl implements PermissionMapper {
