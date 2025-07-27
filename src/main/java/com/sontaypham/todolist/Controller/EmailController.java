f1a7b1c9e6af0c051315be37bd8d41b8700c90ac
sontaypham <phamsontay2206@gmail.com>
2025-07-27 10:15:57 +0700

    update .gitignore

--- src/main/java/com/sontaypham/todolist/Controller/EmailController.java
+++ src/main/java/com/sontaypham/todolist/Controller/EmailController.java
@@ -3,8 +3,8 @@ package com.sontaypham.todolist.Controller;
 import com.sontaypham.todolist.DTO.Response.ApiResponse;
 import com.sontaypham.todolist.DTO.Response.EmailResponse;
 import com.sontaypham.todolist.Entities.EmailDetails;
-import com.sontaypham.todolist.Exception.ApiException;
 import com.sontaypham.todolist.Service.EmailService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,15 +14,16 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 @RestController
 @RequestMapping("/email")
+@RequiredArgsConstructor
 public class EmailController {
-    EmailService emailService;
-    @PostMapping
+    private final EmailService emailService;
+    @PostMapping("/sendSimpleMail")
     public ApiResponse<EmailResponse> sendEmail(@RequestBody EmailDetails emailDetails) {
         try {
-            emailService.sendEmail(emailDetails);
-
+            return ApiResponse.<EmailResponse>builder().status(1).message("Sent email success!").data(emailService.sendSimpleMail(emailDetails)).build();
         }catch (Exception e){
             log.error(e.getMessage());
+            return ApiResponse.<EmailResponse>builder().status(0).message("Sent email failed").data(new EmailResponse(false , "Internal server error")).build();
         }
     }
 }
--- src/main/java/com/sontaypham/todolist/DTO/Response/EmailResponse.java
+++ src/main/java/com/sontaypham/todolist/DTO/Response/EmailResponse.java
@@ -1,12 +1,14 @@
 package com.sontaypham.todolist.DTO.Response;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class EmailResponse {
     boolean success;
     String message;
