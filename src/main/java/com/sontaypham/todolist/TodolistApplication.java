78fc00072ba3b3b4612f4d5738773f2ac0109b82
sontaypham <phamsontay2206@gmail.com>
2025-07-27 16:01:53 +0700

    feat(TaskStatisticsWeekly) : add mail report for task statistics weekly

--- src/main/java/com/sontaypham/todolist/TodolistApplication.java
+++ src/main/java/com/sontaypham/todolist/TodolistApplication.java
@@ -4,10 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import java.time.LocalDateTime;
+
 @SpringBootApplication
 @EnableScheduling
 public class TodolistApplication {
-
   public static void main(String[] args) {
     SpringApplication.run(TodolistApplication.class, args);
   }
